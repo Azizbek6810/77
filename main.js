866e74651080d7a1618c246ef0d0cbc315d0e6d9
Azizbek <a.botirov@rtsb.uz>
2024-10-02 19:30:01 +0500

    icomoon added

--- main.js
+++
@@ -1,8 +0,0 @@
-const userProfile = {}
-
-// const baseUrl = "";
-// const BASE_URL = "https://kun.uz";
-let a = 327483274837284732847328743827483274832;
-let b = 847328472384723874823748237483274832748432;
-let c = a + b;
-console.log(typeof c);
